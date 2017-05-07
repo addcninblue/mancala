88869c98fe3e6f764a803ad2cb7d1e4eddde6229
addcninblue <addison95132@gmail.com>
2017-05-06 18:58:23 -0700

    fixed everything

--- src/AI.java
+++ src/AI.java
@@ -5,96 +5,20 @@ import java.util.List;
  * Created by addison on 5/1/17.
  */
 public class AI implements Player {
-    int[][] board;
+    Board board;
     int playerNumber;
     int weight;
     int depth;
-    ArrayList<Integer> stonesUsed;
-    ArrayList<Boolean> chained; // when one pit ends with another one
-    ArrayList<Integer> initialRows;
-    ArrayList<Integer> finalRows;
-    ArrayList<Integer> finalPositions;
 
-    public AI(int[][] board, int playerNumber){
+    public AI(Board board, int playerNumber){
         this(board, playerNumber, 8, 5);
     }
 
-    public AI(int[][] board, int playerNumber, int weight, int depth){
+    public AI(Board board, int playerNumber, int weight, int depth){
         this.board = board;
         this.playerNumber = playerNumber;
         this.weight = weight;
         this.depth = depth;
-        this.stonesUsed = new ArrayList<>();
-        this.chained = new ArrayList<>();
-        this.initialRows = new ArrayList<>();
-        this.finalRows = new ArrayList<>();
-        this.finalPositions = new ArrayList<>();
-    }
-
-    /**
-     * Moves given the position and row
-     * @param position of pit
-     * @param row playerNumber
-     * @return number of stones removed
-     */
-    public boolean simulateMove(int position, int row){
-        int pitsEach = this.board[0].length - 1;
-        int initialRow = row;
-        int stones = board[row][position];
-        this.stonesUsed.add(0, stones);
-        this.initialRows.add(0, row);
-        board[row][position] = 0;
-        while(stones > 0){
-            position++;
-            if(row != initialRow && position == pitsEach) // if ends up in enemy home
-                position++;
-            row = (row + position / (pitsEach + 1)) % 2;
-            position %= pitsEach + 1;
-            board[row][position]++;
-            stones--;
-        }
-        if(row == initialRow){
-            if(position == 6) { // if ends up in own space
-                chained.add(0, false);
-                finalPositions.add(0, position);
-                this.finalRows.add(0, row);
-                return false;
-            } else if(board[row][position] > 1){
-                chained.add(0, true);
-                finalPositions.add(0, position);
-                this.finalRows.add(0, row);
-                boolean result = simulateMove(position, row);
-                return result;
-            }
-        }
-        chained.add(0, false);
-        finalPositions.add(0, position);
-        this.finalRows.add(0, row);
-        return true;
-    }
-
-    public void undoMoveNew(int row){
-        int numberOfStones = this.stonesUsed.remove(0);
-        int numberOfStonesBackup = numberOfStones;
-        int pitsEach = this.board[0].length;
-        int initialRow = this.initialRows.remove(0);
-        int position = finalPositions.remove(0);
-        row = this.finalRows.remove(0);
-        while(numberOfStones > 0){
-            board[row][position]--;
-            position--;
-            if(row != initialRow && position == pitsEach)
-                position--;
-            row = (row + (pitsEach - position - 1) / pitsEach) % 2;
-            position = (position + pitsEach) % pitsEach;
-            numberOfStones--;
-        }
-        board[row][position] = numberOfStonesBackup; // i apologize
-        this.chained.remove(0);;
-        boolean doAgain = this.chained.size() != 0 && this.chained.get(0);
-        if(doAgain){
-            undoMoveNew(row);
-        }
     }
 
     /**
@@ -115,7 +39,8 @@ public class AI implements Player {
         } else {
             for(int i : nextMoves){
                 // do move
-                boolean goAgain = simulateMove(i, maximize ? playerNumber : (playerNumber + 1) % 2);
+                board.saveState(true);
+                boolean goAgain = board.move(i, maximize ? playerNumber : (playerNumber + 1) % 2, true);
                 if(maximize ^ !goAgain){
                     currentScore = miniMax(depth - 1, false)[0];
                     if(currentScore > bestScore){
@@ -130,7 +55,7 @@ public class AI implements Player {
                     }
                 }
                 // undo move
-                undoMoveNew(maximize ? playerNumber : (playerNumber + 1) % 2);
+                board.undoState();
             }
         }
         return new int[]{bestScore, best};
@@ -138,8 +63,9 @@ public class AI implements Player {
 
     private ArrayList<Integer> generateMoves(int playerNumber){
         ArrayList<Integer> moves = new ArrayList<>();
-        for(int i = 0; i < board[0].length - 1; i++){
-            if(board[playerNumber][i] != 0){
+        int[] row = board.getRow(playerNumber);
+        for(int i = 0; i < row.length - 1; i++){
+            if(row[i] != 0){
                 moves.add(i);
             }
         }
@@ -148,54 +74,45 @@ public class AI implements Player {
 
     private int evaluate(){
         int score = 0;
-        for(int i = 0; i < board[0].length - 1; i++){
-            score += board[playerNumber][i]; // points weighted at 1
-            score -= board[(playerNumber + 1) % 2][i]; // points in opponent
+        int[] mySide = this.board.getRow(playerNumber);
+        int[] opponentSide = this.board.getRow((playerNumber + 1) % 2);
+        for(int i = 0; i < mySide.length - 1; i++){
+            score += (mySide[i] > i + 1) ? -(mySide[i] - i - 1) : mySide[i]; // points weighted at 1
+            score -= (opponentSide[i] > i + 1) ? -(opponentSide[i] - i - 1) : opponentSide[i]; // points in opponent
         }
-        score += board[playerNumber][6] * weight; // points in personal, weighted at 4
-        score -= board[(playerNumber + 1) % 2][6] * weight; // points in opponent
+        score += mySide[6] * weight; // points in personal
+        score -= opponentSide[6] * weight; // points in opponent
         return score;
     }
 
-    private int numOfPieces(){
-        int sum = 0;
-        for(int[] row : board){
-            for(int i : row){
-                sum += i;
-            }
-        }
-        return sum;
-    }
-
-    public void printBoard(){
-        // I apologize for this ugliness
-        // Built with Vim and a lot of patience
-        String str = "     5  4  3  2  1  0\n┌──┬──┬──┬──┬──┬──┬──┬──┐\n│  │%2d│%2d│%2d│%2d│%2d│%2d│  │\n│%2d├──┼──┼──┼──┼──┼──┤%2d│\n│  │%2d│%2d│%2d│%2d│%2d│%2d│  │\n└──┴──┴──┴──┴──┴──┴──┴──┘\n     0  1  2  3  4  5\n";
-        System.out.format(str, board[0][5], board[0][4], board[0][3], board[0][2], board[0][1], board[0][0], board[0][6],
-                board[1][6], board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5]);
-    }
-
-
     @Override
     public int getMove(){
-        int choice = miniMax(2, true)[1]; // debug
-//        int choice = miniMax(depth, true)[1];
-        System.out.println("Computer chose " + choice);
-        return (choice >= 0) ? choice : 0;
+//        int choice = miniMax(5, true)[1]; // debug
+        int[] row = board.getRow(playerNumber);
+        for(int i = row.length - 2; i >= 0; i--){
+            if(row[i] == row.length - i - 1) {
+                System.out.println("Computer" + playerNumber + " chose " + i);
+                return i;
+            }
+        }
+        int[] results = miniMax(depth, true);
+        System.out.println("Computer chose " + results[1]);
+        System.out.println("Advantage: " + results[0]);
+        return (results[1] >= 0) ? results[1] : 0;
     }
 
     @Override
     public String getName(){
-        return "AI";
+        return "Computer";
     }
 
     @Override
     public String getFirstName(){
-        return "AI";
+        return "Computer";
     }
 
     @Override
     public String getLastName(){
-        return "AI";
+        return "Computer";
     }
 }
